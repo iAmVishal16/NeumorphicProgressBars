bc931b4396265626a81699592f12b4c84c3fcf6a
Vishal Paliwal <vishal.paliwal@ril.com>
2023-08-02 10:59:54 +0530

    Initial Commit

---
+++ NeumorphicProgressBars/NeumorphicProgressBarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphicProgressBarsApp.swift
+//  NeumorphicProgressBars
+//
+//  Created by Vishal Paliwal on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphicProgressBarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
